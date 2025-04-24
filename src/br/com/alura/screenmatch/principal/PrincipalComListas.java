05759778f605f7598ab67fd447c850cc84a47197
contato.geovanigaldino@gmail.com <164828104+geovanigaldino@users.noreply.github.com>
2025-04-24 16:14:40 -0300

    "Adiciona material do curso Java: listas e coleções"

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,51 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.*;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(10);
+        var filmeDoGeovani = new Filme("Dogville", 2003);
+        filmeDoGeovani.avalia(7);
+        Serie lost = new Serie("Lost", 2000);
+
+        Serie serie = new Serie("La Casa de Papel", 2017);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(filmeDoGeovani);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+
+        for (Titulo item: lista) {
+            System.out.println("Nome: " + item.getNome());
+            if (item instanceof Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classificação "  + filme.getClassificacao());
+            }
+        }
+
+        List<String> buscaPorArtista = new LinkedList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Paulo");
+        buscaPorArtista.add("Geovani");
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(buscaPorArtista);
+        System.out.println("Depois da ordenação");
+        System.out.println(buscaPorArtista);
+        System.out.println("Lista de títulos ordenados");
+        Collections.sort(lista);
+        System.out.println(lista);
+
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando por ano");
+        System.out.println(lista);
+    }
+}
